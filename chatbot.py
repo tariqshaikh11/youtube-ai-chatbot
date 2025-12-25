35f12ede4feff6f091412b6a2583f368e4b43e5a
tariqshaikh11 <121916425+tariqshaikh11@users.noreply.github.com>
2025-12-25 02:28:32 -0500

    Refactor chatbot.py to include Streamlit integration

--- chatbot.py
+++ chatbot.py
@@ -11,6 +11,7 @@ from langchain_core.runnables import RunnableParallel, RunnablePassthrough, Runn
 from langchain_core.output_parsers import StrOutputParser
 from langchain_google_genai import ChatGoogleGenerativeAI
 from urllib.parse import urlparse, parse_qs
+import streamlit as st
 
 api_key = st.secrets["GOOGLE_API_KEY"]
 
@@ -114,5 +115,6 @@ Question: {question}
             return "⚠ You must submit a video first."
     
         return self.main_chain.invoke(question)
+
 
 
